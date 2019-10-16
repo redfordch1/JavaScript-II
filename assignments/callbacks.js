a9bb317b1ee24b234ab3bd9ae5e47488278b71f2
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2019-10-15 22:45:09 -0600

    JS II

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -8,6 +8,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
+    return cb(arr[0]);
   }
 
   // SOLUTION:
@@ -41,23 +42,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length -1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  const sum = x + y;
+  return cb(sum);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  const answer = x * y;
+  return cb(answer);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (i = 0; i < list.length; i++){
+    if (list[i] === item){
+      result = true;
+    } 
+    else result = false;
+  }
+  return cb(result);
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +80,5 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+    cb(array.from(new Set(array)));
 }
